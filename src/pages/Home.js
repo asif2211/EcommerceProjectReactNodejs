041becc962af2be332c3fb9a20abf6c27cbd4d43
asif2211 <asifbsit@gmail.com>
2020-11-14 13:50:35 +0500

    ecommerce project

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Hom extends Component {
+    render() {
+        return (
+            <div>
+                Home page
+            </div>
+        )
+    }
+}
